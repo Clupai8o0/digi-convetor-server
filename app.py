73b75faa0c1f17ed9703e26d93582b92c3c070f4
Clupai8o0 <clupaio4@gmail.com>
2022-08-15 09:55:05 +0300

    cors policy for api

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ def home():
 
 @app.route("/convert/<string:typeFrom>/<string:typeTo>/<string:value>", methods=['GET'])
 def convert(typeFrom, typeTo, value):
-  return jsonify(
+  response = jsonify(
     {
       "from": typeFrom,
       "to": typeTo,
       "value": value
     }
   )
+  response.headers.add("Access-Control-Allow-Origin", "*")
+
+  return response
 
 app.run(host="192.168.43.239", port=5000, debug=True)
